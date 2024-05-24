62747a12ab10d63c587b4c2c141d45991f0e1b2c
meg <62103855+meg-hz@users.noreply.github.com>
2024-05-24 11:34:01 +0530

    Updated PockGet.py.
    Added module to run single pocket tool at a time

--- PockGet.py
+++ PockGet.py
@@ -1,6 +1,6 @@
 # PockGet.py
-# single hashes for info. double hashes for debugging/alternate code
 # code to run fpocket, sitehound and pocketdepth for files in a given a list 
+# single hashes for info. double hashes for debugging/alternate code
 
 # IMPORTANT: this code works IF sitehound, fpocket & pocketdepth folders
 # as well as the directory with input proteins and this code are in the same root folder
@@ -116,7 +116,7 @@ def main(pdb_paths, accession_list=None):
                 if file not in accession_list:
                     accession_list.append(file.strip())
         
-    output_path= os.path.join(root,'protein_result') 
+    output_path= os.path.join(root,'PocketResults') 
     if not os.path.exists(output_path):
         os.mkdir(output_path)
 
@@ -146,45 +146,85 @@ def main(pdb_paths, accession_list=None):
                 pocketdepth(file_address, new_folder)  
             
 
+def main2(pdb_paths, mode, accession_list=None):
+    
+    if accession_list==None: # basically accounts for the all protein files in the path if a list is not given
+        accession_list = list(set([file for file in os.listdir(pdb_paths) if os.path.isfile(os.path.join(pdb_paths,file)) and file.endswith('.pdb')]))
+    
+    output_path= os.path.join(root,f"{mode[2:]}_results")
+    count=0
+    while os.path.exists(output_path):
+        count+=1
+        output_path= os.path.join(root,f"{mode[2:]}_results({count})")
+    os.mkdir(output_path)
+
+    def themode(pockmode,filepath,op_path):
+        if pockmode == '--fpocket':
+            return fpocket(filepath,op_path)
+        elif pockmode == '--sitehound':
+            return sitehound(filepath,op_path)
+        elif pockmode == '--pocketdepth':
+            return sitehound(filepath,op_path)
+
+
+    for accession in accession_list:
+        for file in os.listdir(pdb_paths): 
+            if file.endswith('.pdb') and accession in file:
+                file_address=os.path.join(pdb_paths,file)
+                ##print(file_address)
+                ##print()
+
+                protein_folder=os.path.join(output_path,f'{file[:-4]}_fpk') 
+                themode(mode,file_address,protein_folder)
+
 #---------------------------------------------------------------------------------------------------
-# MAIN PROGRAM STARTS HERE
+# TERMINAL MODULE STARTS HERE
 
 if __name__ == "__main__":
 
-    if len(sys.argv)==4 and sys.argv[1] =='--list':
-        in_dir= sys.argv[2] # path to folder containing input pdb files 
-        file_list= sys.argv[3] # path to text file with list of desired accession numbers
+    if (len(sys.argv)==4 and sys.argv[2]=='--dir') or (len(sys.argv)==5 and sys.argv[2]=='--list'):
 
-        if not os.path.isdir(in_dir):
-            print("Invalid PDB dir path")
+        # accounting for folder containing PDB files
+        input_dir=sys.argv[3]
+        if not os.path.isdir(input_dir):
+            print("Invalid PDB dir path. Exiting Code...")
             exit(1)
-    
-        accession_list=[]
-        if os.path.isfile(file_list) and file_list.endswith(('.txt', '.tsv', '.csv')):
-            with open(file_list, 'r') as ref:  # this works if file_list is a file with a list of filenames
-                for line in ref:
-                    if line.split()[0] not in accession_list:
-                        accession_list.append(line.split('\n')[0]) # array of file names
-                        ##print(accession_list)
+
+        # accounting for list of protein accession numbers
+        if len(sys.argv)==4:
+            accession_list=None
         else:
-            accession_list.append(file_list.strip())
+            file_list=sys.argv[4]
+
+            if not os.path.isfile(file_list) and file_list.endswith('.txt'):
+                print("Invalid Reference File Path. Exiting Code...")
+                exit(1)
+
+            with open(file_list, 'r') as ref:
+                lines=ref.readlines()
+            accession_list=sorted(list(set([i.split()[0] for i in lines])))
+
+        # accounting for mode
+        runmode= sys.argv[1]
+
+        if runmode== '--all':
+            main(input_dir,accession_list)
+        elif runmode in ('--fpocket','--sitehound','--pocketdepth'):
+            main2(input_dir,runmode,accession_list)       
         
-        main(in_dir,accession_list)
-
-        
-    elif len(sys.argv)==3 and sys.argv[1]=='--all':
-        in_dir= sys.argv[2] # path to folder containing input pdb files 
-
-        if not os.path.isdir(in_dir):
-            print("Invalid PDB dir path")
-            exit(1)
-
-        main(in_dir)  
 
     else:
         print("PockGet.py → USAGE:")
-        print("For a list of files:")
-        print("\tpython PockGet.py --list <path to input dir> <protein filename>")
-        print("\tpython PockGet.py --list <path to input directory> <path to text file with all filenames>")
-        print("For all files in the folder:")
-        print("\tpython PockGet.py --all <path to input dir>")
+        print("To run all three pocket prediction tools ")
+        print("\tFor a list of files:")
+        print("\t\tpython PockGet.py --all --list <path to input directory> <path to reference file>")
+        print("\tFor all files in the folder:")
+        print("\t\tpython PockGet.py --all --dir <path to input dir>")
+        print()
+        print("To run a specific pocket prediction tool ")
+        print("(replace <mode> with '--fpocket','--sitehound' or '--pocketdepth')")
+        print("\tFor a list of files:")
+        print("\t\tpython PockGet.py <mode> --list <path to input directory> <path to reference file>")
+        print("\tFor all files in the folder:")
+        print("\t\tpython PockGet.py <mode> --dir <path to input dir>")
+        print()
